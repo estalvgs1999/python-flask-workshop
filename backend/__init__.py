6d1f06494f787065ea79d0b2cf0653aba2c0719f
Esteban Alvarado <estalvgs1999@estudiantec.cr>
2022-09-07 13:06:09 -0600

    [add] error handlers

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from backend.routes.health import health_bp
 from backend.routes.users import users_bp
+from backend.routes.error import error_bp
 
 
 def create_app():
@@ -8,5 +9,6 @@ def create_app():
 
     app.register_blueprint(health_bp)
     app.register_blueprint(users_bp)
+    app.register_blueprint(error_bp)
 
     return app
